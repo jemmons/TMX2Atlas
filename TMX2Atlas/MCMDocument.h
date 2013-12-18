ee6833400afd7c203b16ea7cc88a768802770104
Joshua Emmons <skia@skia.net>
2013-12-17 22:04:40 -0500

    Initial Commit

---
+++ TMX2Atlas/MCMDocument.h
@@ -0,0 +1,13 @@
+//
+//  MCMDocument.h
+//  TMX2Atlas
+//
+//  Created by Joshua Emmons on 12/17/13.
+//  Copyright (c) 2013 MCMXCIX. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MCMDocument : NSDocument
+
+@end
---
+++ TMX2Atlas/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TMX2Atlas
+//
+//  Created by Joshua Emmons on 12/17/13.
+//  Copyright (c) 2013 MCMXCIX. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+  return NSApplicationMain(argc, argv);
+}
